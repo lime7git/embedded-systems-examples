7cb0f53f546f3e15eed15182c66be0e900e53a27
lime7git <e.bugajski7@gmail.com>
2022-03-05 17:14:41 +0100

    Update test_main.c

--- STM32/stm32f401/nucleo-board/blink-unit-testing/test/test_main.c
+++ STM32/stm32f401/nucleo-board/blink-unit-testing/test/test_main.c
@@ -2,7 +2,7 @@
 #include <unity.h>
 
 void setUp(void) {
-     LED_GPIO_CLK_ENABLE();
+    LED_GPIO_CLK_ENABLE();
     GPIO_InitTypeDef GPIO_InitStruct;
     GPIO_InitStruct.Pin = LED_PIN;
     GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
